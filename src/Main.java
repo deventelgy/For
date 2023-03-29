cb9d2d30d5ca8b73fc42d26213c8f805716b8585
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-29 09:49:12 -0500

    Uso de For

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //Definimos el bucle, incluye el 100
+        for (int num=1;num<=100;num++){
+            System.out.println(num);
+        }
+    }
+}
